a8caeb8b05f7982e8f95980798af7c3188302dbf
Eddycarlos45 <edsoncarlos33@hotmail.com>
2020-01-27 15:34:37 -0300

    Adiciona formLogin

--- 4Tech-frontend/4tech-frontend/src/App.js
+++ 4Tech-frontend/4tech-frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './containers/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      App works!!
+      <Login/>
     </div>
   );
 }
