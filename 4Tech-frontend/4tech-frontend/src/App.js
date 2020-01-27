a674bb2dc57adca5f830cd8f7c22f55f19c21a91
Eddycarlos45 <edsoncarlos33@hotmail.com>
2020-01-27 16:38:28 -0300

    Adiciona Rotas

--- 4Tech-frontend/4tech-frontend/src/App.js
+++ 4Tech-frontend/4tech-frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import Login from './containers/Login/Login'
+import Login from './containers/Login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/timeline" component={Login} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
--- 4Tech-frontend/4tech-frontend/src/services/auth.js
+++ 4Tech-frontend/4tech-frontend/src/services/auth.js
@@ -1,2 +1,22 @@
-import {axios4tech} from './config';
+import { axios4tech } from './config';
 
+export const login = async (userLogin, password) => {
+	try {
+		const response = await axios4tech.post('auth/login', {
+			userLogin,
+			password
+		});
+		if (response.status >= 200 && response.status < 300) {
+			localStorage.setItem('token', response.data.acess_token);
+			localStorage.setItem('userId', response.date._id);
+			return response;
+		}
+	} catch (e) {
+		return e;
+	}
+}
+
+export const logout = () => {
+	localStorage.removeItem('token');
+	localStorage.removeItem('userId');
+}
